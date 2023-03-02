a3ac209998d8fb241518c1d92e56e92a229c2b59
Dayane <day.nemesis@gmail.com>
2023-03-02 15:25:22 -0300

    atualizações dos códigos de aula

---
+++ estrutura_repeticao_break.py
@@ -0,0 +1,15 @@
+# while True:
+#     numero = int(input("Informe um número: "))
+    
+#     if numero == 10:
+#         break
+    
+#     print(numero)
+
+
+for numero in range(100):
+    
+    if numero == 12:
+        break  #a variação do break é o comando continue que pula a execução do código
+    
+    print(numero, end=" ")
